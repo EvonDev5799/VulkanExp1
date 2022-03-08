14b541ec2eecbb6c890c21b02bc20893ce4c4149
Evonis57 <51177314+Evonis57@users.noreply.github.com>
2022-03-08 19:51:02 +0100

    modified compilation process into build folder

--- compile.bat
+++ compile.bat
@@ -1,3 +1,4 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
-cl source.c gdi32.lib user32.lib /std:c17 /Wall
+cd build
+cl ../source.c gdi32.lib user32.lib /std:c17 /Wall
 pause
