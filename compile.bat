9627a3ea48e0d2ad9cf0a192d411b6f9bf3ad9ba
Evonis57 <51177314+Evonis57@users.noreply.github.com>
2022-03-10 15:18:49 +0100

    added first vulkan dependencies and a function for testing

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 mkdir build
 cd build
-cl ../source.c ../utilityWindow.c gdi32.lib user32.lib /std:c17 /W4
+cl ../source.c ../utilityWindow.c gdi32.lib user32.lib C:\VulkanSDK\1.3.204.1\Lib\vulkan-1.lib /std:c17 /W4
 pause
--- source.c
+++ source.c
@@ -1,10 +1,13 @@
 #include "utilityWindow.h"
+#include "C:\VulkanSDK\1.3.204.1\Include\vulkan\vulkan.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 				   LPSTR lpCmdLine, int nCmdShow)
 {
 	createConsole(L"console");
 	createWindow(L"window", 1024, 512, 0, 0, hInstance);
+	int i;
+	vkEnumerateInstanceExtensionProperties(NULL, &i, NULL);
 	renderLoop();
 	return(0);
 }
